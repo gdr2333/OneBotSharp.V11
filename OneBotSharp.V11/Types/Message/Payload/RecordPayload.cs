aeec40798d80ae6a2e6eb876f24e632ffcd7f7b4
df1050 <df1050@126.com>
2025-04-29 14:39:05 +0800

    实现录音字段 格式更改

--- OneBotSharp.V11/Types/Message/Payload/RecordPayload.cs
+++ OneBotSharp.V11/Types/Message/Payload/RecordPayload.cs
@@ -4,13 +4,14 @@ using OneBotSharp.V11.Utils.Converter;
 namespace OneBotSharp.V11.Types.Message.Payload;
 
 [method: JsonConstructor]
-internal struct ImagePayload() : IPayload<ImagePayload>
+internal struct RecordPayload() : IPayload<RecordPayload>
 {
+    // 基本就是从ImagePayload改的，也许我该写个FilePayload的抽象？
     [JsonInclude, JsonRequired, JsonPropertyName("file")]
     public string File;
 
-    [JsonInclude, JsonPropertyName("type")]
-    public string? ImageType = null;
+    [JsonInclude, JsonPropertyName("magic"), JsonConverter(typeof(CqBoolConverter))]
+    public bool UseMagic = false;
 
     [JsonInclude, JsonPropertyName("url")]
     public Uri? Url;
@@ -24,11 +25,11 @@ internal struct ImagePayload() : IPayload<ImagePayload>
     [JsonInclude, JsonConverter(typeof(CqIntConverter))]
     public int? TimeOut = null;
 
-    public static ImagePayload Create(CqCode code) =>
+    public static RecordPayload Create(CqCode code) =>
         new()
         {
             File = code.Payload["file"],
-            ImageType = code.Payload.TryGetValue("type", out var typ) ? typ : null,
+            UseMagic = code.Payload.TryGetValue("magic", out var typ) && CqCodeUtil.CqBoolDecode(typ),
             Url = code.Payload.TryGetValue("url", out var url) ? new(url) : null
         };
 }
