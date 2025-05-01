1b62b2919edc480bd63df26ab3e8549445036f5d
df1050 <df1050@126.com>
2025-05-01 11:05:43 +0800

    不对不对我搞错了

--- OneBotSharp.V11/Types/Message/AnonymousSegment.cs
+++ OneBotSharp.V11/Types/Message/AnonymousSegment.cs
@@ -14,12 +14,30 @@ public class AnonymousSegment : Segment
 
     /// <inheritdoc/>
     [JsonInclude, JsonPropertyName("data")]
-    public override JsonNode Payload => new JsonObject();
+    public override JsonNode Payload => _payload;
+
+    /// <summary>
+    /// 是否在无法匿名时继续发送
+    /// </summary>
+    public bool Ignore { get; private init; }
+
+    private JsonNode _payload;
 
     /// <summary>
     /// 新建一个匿名消息段
     /// </summary>
-    public AnonymousSegment() 
+    public AnonymousSegment(bool ignore)
     {
+        Ignore = ignore;
+        _payload = new JsonObject()
+        {
+            {"ignore", CqCodeUtil.CqBoolEncoode(Ignore)}
+        };
+    }
+
+    internal AnonymousSegment()
+    {
+        _payload = new JsonObject();
+        Ignore = false;
     }
 }
