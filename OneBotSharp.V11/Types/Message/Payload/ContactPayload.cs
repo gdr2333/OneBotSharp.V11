8e0b01540f049db8835cf039349beb7621c0c0e0
df1050 <df1050@126.com>
2025-05-01 20:59:02 +0800

    添加各种推荐消息段

---
+++ OneBotSharp.V11/Types/Message/Payload/ContactPayload.cs
@@ -0,0 +1,20 @@
+﻿using OneBotSharp.V11.Types.Message.Base;
+using OneBotSharp.V11.Utils.Converter;
+
+namespace OneBotSharp.V11.Types.Message.Payload;
+
+internal struct ContactPayload : IPayload<ContactPayload>
+{
+    [JsonInclude, JsonRequired, JsonPropertyName("type")]
+    public string Type;
+
+    [JsonInclude, JsonRequired, JsonPropertyName("id"), JsonConverter(typeof(CqLongConverter))]
+    public long Id;
+
+    public static ContactPayload Create(CqCode code) =>
+        new()
+        {
+            Type = code.Payload["type"],
+            Id = long.Parse(code.Payload["id"], Defaults.DefaultFormat)
+        };
+}
