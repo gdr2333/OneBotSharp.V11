19f9c22206fec911c4b06c2b09c703b415725e49
df1050 <df1050@126.com>
2025-05-01 11:00:18 +0800

    新增匿名消息段

---
+++ OneBotSharp.V11/Types/Message/AnonymousSegment.cs
@@ -0,0 +1,25 @@
+﻿using OneBotSharp.V11.Types.Message.Base;
+
+namespace OneBotSharp.V11.Types.Message;
+
+/// <summary>
+/// 匿名消息段
+/// </summary>
+public class AnonymousSegment : Segment
+{
+    // 因为收不到这下连个内部的payload都无了
+    /// <inheritdoc/>
+    [JsonInclude, JsonPropertyName("type")]
+    public override string Type => "anonymous";
+
+    /// <inheritdoc/>
+    [JsonInclude, JsonPropertyName("data")]
+    public override JsonNode Payload => new JsonObject();
+
+    /// <summary>
+    /// 新建一个匿名消息段
+    /// </summary>
+    public AnonymousSegment() 
+    {
+    }
+}
