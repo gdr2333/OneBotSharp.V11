2e31a7198682c6ec47655935fb187d7b94bc5442
df1050 <df1050@126.com>
2025-05-02 14:43:40 +0800

    添加位置分享消息段

---
+++ OneBotSharp.V11/Types/Message/Payload/LocationPayload.cs
@@ -0,0 +1,28 @@
+﻿using OneBotSharp.V11.Types.Message.Base;
+using OneBotSharp.V11.Utils.Converter;
+
+namespace OneBotSharp.V11.Types.Message.Payload;
+
+internal struct LocationPayload : IPayload<LocationPayload>
+{
+    [JsonInclude, JsonRequired, JsonPropertyName("lat"), JsonConverter(typeof(CqDoubleConverter))]
+    public double Latitude { get; set; }
+
+    [JsonInclude, JsonRequired, JsonPropertyName("lon"), JsonConverter(typeof(CqDoubleConverter))]
+    public double Longitude { get; set; }
+
+    [JsonInclude, JsonPropertyName("title")]
+    public string? Title;
+
+    [JsonInclude, JsonPropertyName("content")]
+    public string? Content;
+
+    public static LocationPayload Create(CqCode code) =>
+        new()
+        {
+            Latitude = double.Parse(code.Payload["lat"], Defaults.DefaultFormat),
+            Longitude = double.Parse(code.Payload["lon"], Defaults.DefaultFormat),
+            Title = code.Payload.TryGetValue("title", out var title) ? title : null,
+            Content = code.Payload.TryGetValue("content", out var content) ? content : null,
+        };
+}
