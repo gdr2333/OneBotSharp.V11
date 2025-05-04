259b84a56ee8161439c59dff7b7ee546559a22eb
df1050 <df1050@126.com>
2025-05-04 12:54:21 +0800

    JSON消息段和XML消息段

---
+++ OneBotSharp.V11/Types/Message/XmlSegment.cs
@@ -0,0 +1,50 @@
+﻿using System.Xml;
+using OneBotSharp.V11.Types.Message.Base;
+using OneBotSharp.V11.Types.Message.Payload;
+
+namespace OneBotSharp.V11.Types.Message;
+
+/// <summary>
+/// XML消息段
+/// </summary>
+public class XmlSegment : Segment
+{
+    /// <inheritdoc/>
+    public override string Type => "xml";
+
+    /// <inheritdoc/>
+    public override JsonNode Payload => _payload;
+
+    /// <summary>
+    /// 消息段的Data数据（XML）
+    /// </summary>
+    public XmlDocument Data { get; private init; }
+
+    private readonly JsonNode _payload;
+
+    /// <summary>
+    /// 使用XML数据构建新的XML消息段
+    /// </summary>
+    /// <param name="data">XML数据</param>
+    public XmlSegment(XmlDocument data)
+    {
+        Data = data;
+        // 我知道这一点都不优雅，但我只会这么干。
+        using StringWriter writer = new();
+        using XmlTextWriter xmlWriter = new(writer);
+        data.WriteTo(xmlWriter);
+        xmlWriter.Flush();
+        _payload = new JsonObject()
+        {
+            { "data", writer.ToString() }
+        };
+    }
+
+    internal XmlSegment(DataOnlyPayload payload, JsonNode payloadNode)
+    {
+        using StringReader reader = new(payload.Data);
+        Data = new();
+        Data.Load(reader);
+        _payload = payloadNode;
+    }
+}
